df0e91fd630a48e8a8dfdbbd29f6271772d6a965
geraldandreiv <g.villa.141963.tc@uminndanao.edu.ph>
2025-12-21 20:01:23 +0800

    C:\Users\Admin\Documents\FarmDashr\FarmDashr\lib\blocs\product\product_event.dart

---
+++ lib/blocs/product/product_event.dart
@@ -0,0 +1,55 @@
+import 'package:equatable/equatable.dart';
+import 'package:farmdashr/data/models/product.dart';
+
+/// Base class for all product events.
+abstract class ProductEvent extends Equatable {
+  const ProductEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+/// Event to load all products from repository.
+class LoadProducts extends ProductEvent {
+  const LoadProducts();
+}
+
+/// Event to add a new product.
+class AddProduct extends ProductEvent {
+  final Product product;
+
+  const AddProduct(this.product);
+
+  @override
+  List<Object?> get props => [product];
+}
+
+/// Event to update an existing product.
+class UpdateProduct extends ProductEvent {
+  final Product product;
+
+  const UpdateProduct(this.product);
+
+  @override
+  List<Object?> get props => [product];
+}
+
+/// Event to delete a product by ID.
+class DeleteProduct extends ProductEvent {
+  final String productId;
+
+  const DeleteProduct(this.productId);
+
+  @override
+  List<Object?> get props => [productId];
+}
+
+/// Event to search products by query.
+class SearchProducts extends ProductEvent {
+  final String query;
+
+  const SearchProducts(this.query);
+
+  @override
+  List<Object?> get props => [query];
+}
