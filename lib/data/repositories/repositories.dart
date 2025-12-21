19569426d6b8d3b0eedeb60d52a7c169a9acb289
geraldandreiv <g.villa.141963.tc@uminndanao.edu.ph>
2025-12-21 18:47:25 +0800

    Barrel file for easy imports

---
+++ lib/data/repositories/repositories.dart
@@ -0,0 +1,12 @@
+/// Repository exports for easy importing.
+///
+/// Usage:
+/// ```dart
+/// import 'package:farmdashr/data/repositories/repositories.dart';
+/// ```
+library;
+
+export 'base_repository.dart';
+export 'order_repository.dart';
+export 'product_repository.dart';
+export 'user_repository.dart';
