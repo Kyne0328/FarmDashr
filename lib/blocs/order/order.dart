1bfe49eb577ca65367f6ce5931627e228fc2c6b5
geraldandreiv <g.villa.141963.tc@uminndanao.edu.ph>
2025-12-23 13:48:05 +0800

    Barrel file for clean imports

---
+++ lib/blocs/order/order.dart
@@ -0,0 +1,11 @@
+/// Order BLoC - exports all order bloc components.
+///
+/// Usage:
+/// ```dart
+/// import 'package:farmdashr/blocs/order/order.dart';
+/// ```
+library;
+
+export 'order_bloc.dart';
+export 'order_event.dart';
+export 'order_state.dart';
