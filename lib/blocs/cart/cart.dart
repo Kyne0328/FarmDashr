90d3431e2869cf6cf55091c05bea323211e45330
geraldandreiv <g.villa.141963.tc@uminndanao.edu.ph>
2025-12-23 14:05:57 +0800

    Barell file for easy importing

---
+++ lib/blocs/cart/cart.dart
@@ -0,0 +1,11 @@
+/// Cart BLoC - exports all cart bloc components.
+///
+/// Usage:
+/// ```dart
+/// import 'package:farmdashr/blocs/cart/cart.dart';
+/// ```
+library;
+
+export 'cart_bloc.dart';
+export 'cart_event.dart';
+export 'cart_state.dart';
