ce6ebe0406f734f38112ff66b94112956893cfe9
geraldandreiv <g.villa.141963.tc@uminndanao.edu.ph>
2025-12-23 14:19:40 +0800

    barell file for importing

---
+++ lib/blocs/auth/auth.dart
@@ -0,0 +1,11 @@
+/// Auth BLoC - exports all auth bloc components.
+///
+/// Usage:
+/// ```dart
+/// import 'package:farmdashr/blocs/auth/auth.dart';
+/// ```
+library;
+
+export 'auth_bloc.dart';
+export 'auth_event.dart';
+export 'auth_state.dart';
